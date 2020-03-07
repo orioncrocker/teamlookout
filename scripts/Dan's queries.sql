08796ccb5b7dcfa710f27849e127f47ad23e23fd
Daniel Brewer <44036901+DanielBrewerPsu@users.noreply.github.com>
2020-03-07 08:42:01 -0800

    Added North+Southbound to data for Dan's presentation graphs
     - Used "distinct crashid" instead of crashid in all queries
     - Added North and South direction. Previous proof-of-concept was just all crashes
     - Changed all queries and column names to Thousand Hours instead of Hours
     - updated csv data to match queries

--- scripts/Dan's queries.sql
+++ scripts/Dan's queries.sql
@@ -1,8 +1,23 @@
---- Here's how to join crashes and weather in Postgres, though I ended up not using this
+--- Practicing how to join crashes and weather
 select crashid, weather.summary
 from i5north_bycrash crashes join weather on (crashes.date + crashes.hour = weather.datetime)
 group by crashid, weather.summary
 
+
+-- Count crashes per milepost to normalize some data
+select count(distinct crashid) as crashCount, milepost, 'North' as direction
+into crashesperMilepost 
+from i5north_bycrash 
+group by milepost
+order by milepost;
+-- ...and add Southbound
+insert into crashesperMilepost
+select count(distinct crashid)as crashCount, milepost, 'South' as direction
+from i5south_bycrash 
+group by milepost
+order by milepost;
+-- Sanity check... there should be 694 unique crashes
+select sum(crashCount) from crashesperMilepost;
 
 
 
@@ -16,16 +31,36 @@ order by summary
 
 
 --- Get Crashes/hour with each weather type
-select milepost, weather1, (CrashCount / countofhourswithweathertype.countofhourswithweather1type) as CrashesPerHour
+select milepost, weather1, 'North' as direction, (1000 * CrashCount / countofhourswithweathertype.countofhourswithweather1type) as CrashesPerThousandHours
 into CrashesPerHourByWeatherByMilepost
 from (
-	select milepost, (count(crashid) + 0.0000001) as CrashCount, weather1
+	select milepost, (count(distinct crashid) + 0.0000000001) as CrashCount, weather1
 	from i5north_bycrash 
 	group by milepost, weather1
 	order by milepost) as CrashCounts join countofhourswithweathertype on weather1 = weathersummary
-order by milepost, weather1
-	
-	
+order by milepost, weather1;
+--- ...and Southbound
+insert into CrashesPerHourByWeatherByMilepost
+select milepost, weather1, 'South' as direction, 
+	(1000 * CrashCount / countofhourswithweathertype.countofhourswithweather1type) as CrashesPerThousandHours
+from (
+	select milepost, (count(distinct crashid) + 0.0000000001) as CrashCount, weather1
+	from i5south_bycrash 
+	group by milepost, weather1
+	order by milepost) as CrashCounts join countofhourswithweathertype on weather1 = weathersummary
+order by milepost, weather1;
+---
+
+
+
+--- This tried and failed to find "hotspots" for specific weather types. There should be 
+--- some calculation that normalizes a number of crashes using the avg crashes for that 
+--- weather and the avg crashes at that milepost. But... I never found it.
+select milepost, weather1, direction, (byWeather.CrashesPerThousandHours / byMilepost.crashCount) as correctedCrashCount
+into crashesperhourcorrectedbymilepost
+from CrashesPerHourByWeatherByMilepost as byWeather natural join crashesperMilepost as byMilepost
+order by milepost, direction;
+
 	
 
 --- Count number of hours with each visibility level
@@ -38,12 +73,22 @@ order by visibility
 
 
 --- Get Crashes/hour with each visibility level
-select milepost, visibility as VisibilityLevel, (CrashCount / CountOfHoursWithVisibilityLevel.CountOfHours) as CrashesPerHour
+select milepost, visibility, 'North' as direction, 
+	(1000 * CrashCount / CountOfHoursWithVisibilityLevel.CountOfHours) as CrashesPerThousandHours
 into CrashesPerHourByVisibilityLevelAndMilepost
 from (
-	select milepost, (count(crashid) + 0.0000001) as CrashCount, visibility
+	select milepost, (count(distinct crashid) + 0.0000001) as CrashCount, visibility
 	from i5north_bycrash 
 	group by milepost, visibility
 	order by milepost) as CrashCounts natural join CountOfHoursWithVisibilityLevel
-order by milepost, visibility
-
+order by milepost, visibility;
+--- add Southbound
+insert into CrashesPerHourByVisibilityLevelAndMilepost
+select milepost, visibility, 'South' as direction, 
+	(1000 * CrashCount / CountOfHoursWithVisibilityLevel.CountOfHours) as CrashesPerThousandHours
+from (
+	select milepost, (count(distinct crashid) + 0.0000001) as CrashCount, visibility
+	from i5south_bycrash 
+	group by milepost, visibility
+	order by milepost) as CrashCounts natural join CountOfHoursWithVisibilityLevel
+order by milepost, visibility;
