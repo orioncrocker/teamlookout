f8041bfeaa075544390ae9b2e6fa46f1bbab9791
Seth Seeman <sseeman@pdx.edu>
2020-03-17 21:42:29 -0700

    final presentation

---
+++ scripts/tw2016schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tw2016_update (
+    date date,
+	time time,
+	day varchar(10),
+    volume numeric,
+    speed numeric,
+    highwayid integer,
+    milepost double precision,
+    lat double precision,
+    lon double precision,
+    weather_desc character varying(20),
+    precip real,
+    temp real,
+    windspeed real,
+    windgust real,
+    cloudcover real,
+    visibility integer
+);
